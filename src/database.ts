cd88389eb4b0bd796132417ccdb3a960df0de93a
Barbosa <daniel.barbosa@capgemini.com>
2023-04-09 18:49:57 -0300

    chore: Ajuste para deploy

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,5 @@
-import { knex as setupKnex, Knex } from 'knex'
+import 'dotenv/config'
+import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
